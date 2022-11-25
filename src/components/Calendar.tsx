95ec43e7d09bc00540cc4761bdb45afa4d239f74
donot1 <kalghatgin@gmail.com>
2022-11-25 11:50:39 -0600

    tried highlighting days

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,23 @@
 import Calendar from 'react-calendar';
-
+import 'react-calendar/dist/Calendar.css';
 // TODO: add type
-const VoiceCalendar = ({ selectedDate, setSelectedDate }: any) => {
+
+
+
+const VoiceCalendar = ({ selectedDate, setSelectedDate, memoryDates }: any) => {
+  const tileClassName = ({ date, view }: any ) => {
+    if (memoryDates.find(({memdate} : any) => memdate === date)) {
+      return 'highlightedDates';
+    }
+    return ''; 
+  }
+  
+  
   return (
     <div className="mx-6">
-      <Calendar value={selectedDate} onChange={setSelectedDate} />
+      <Calendar value={selectedDate}
+                onChange={setSelectedDate}
+                tileClassName={tileClassName} />
     </div>
   );
 };
